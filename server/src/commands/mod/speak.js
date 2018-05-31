bf2d064679e2253868a3e321548337a8d3065726
OpSimple <35542206+OpSimple@users.noreply.github.com>
2018-06-01 01:15:45 +0530

    Created speak.js

---
+++ server/src/commands/mod/speak.js
@@ -0,0 +1,43 @@
+/* 
+ * Description: Pardon a dumb user to be able to speak again
+ * Author: simple
+ */
+
+
+exports.run = async (core, server, socket, data) => {
+  if (socket.uType == 'user') {
+    // ignore if not mod or admin
+    return;
+  }
+
+  if (typeof data.ip !== 'string' && typeof data.hash !== 'string') {
+    server.reply({
+      cmd: 'warn',
+      text: "hash:'targethash' or ip:'1.2.3.4' is required"
+    }, socket);
+
+    return;
+  }
+  
+  let target;
+
+  if (typeof data.ip === 'string') {
+    target = getSocketHash(data.ip);
+  } else {
+    target = data.hash;
+  }
+  
+  delete core.muzzledHashes[target];
+  
+  server.broadcast({
+    cmd: 'info',
+    text: `${socket.nick} unmuzzled : ${target}`
+  }, { uType: 'mod' });
+  
+}
+
+exports.info = {
+  name: 'speak',
+  usage: 'speak {[ip || hash]}',
+  description: 'Pardon a dumb user to be able to speak again'
+};
