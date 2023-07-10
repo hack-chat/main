fb67f5d73aa31704619c96d70ce6b0e317939789
Maggie <96947247+MaggieLOL@users.noreply.github.com>
2023-07-10 12:41:43 +0800

    Change the rate-limit score

--- server/src/commands/core/ping.js
+++ server/src/commands/core/ping.js
@@ -4,7 +4,7 @@
 
 // module main
 export async function run() {
-  if (server.police.frisk(socket.address, 30)) {
+  if (server.police.frisk(socket.address, 1)) {
     return server.reply({
       cmd: 'warn',
       text: 'You are pinging too fast. Wait a moment before trying again.',
